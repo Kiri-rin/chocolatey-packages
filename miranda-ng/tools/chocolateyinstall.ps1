6de5a03c8fda8b5a6204c7aa35df4b62702df7d3
Kiririn <ritsufag@gmail.com>
2015-07-27 01:15:31 +0300

    Change silent to verysilent

--- miranda-ng/tools/chocolateyinstall.ps1
+++ miranda-ng/tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@
   url           = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4.exe'
   url64bit      = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4_x64.exe'
   validExitCodes= @(0)
-  silentArgs    = '/SILENT'
+  silentArgs    = '/verysilent'
 }
 
 Install-ChocolateyPackage @packageArgs
