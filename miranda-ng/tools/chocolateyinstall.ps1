4f26e18f253fc508fff1ca21d4514f02f2e169d8
Kiririn <ritsufag@gmail.com>
2015-07-26 23:18:40 +0300

    Add Miranda NG package

---
+++ miranda-ng/tools/chocolateyinstall.ps1
@@ -0,0 +1,10 @@
+﻿$packageArgs = @{
+  packageName   = 'miranda-ng'
+  fileType      = 'exe'
+  url           = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4.exe'
+  url64bit      = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4_x64.exe'
+  validExitCodes= @(0)
+  silentArgs    = '/S'
+}
+
+Install-ChocolateyPackage @packageArgs
