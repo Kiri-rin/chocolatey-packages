0d9ecdf6854788dc6e7758a93c6e7353e00b4b55
Kiririn <ritsufag@gmail.com>
2015-07-27 01:27:55 +0300

    Add miranda-ng uninstaller

---
+++ miranda-ng/tools/chocolateyUninstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageArgs = @{
+  packageName   = 'miranda-ng'
+  fileType      = 'exe'
+  validExitCodes= @(0)
+  unpath        = "$Env:ProgramFiles\Miranda NG\Uninstall\unins000.exe"
+  silentArgs    = '/verysilent'
+}
+
+try {
+  Uninstall-ChocolateyPackage @packageArgs
+  Write-ChocolateySuccess $packageName
+} 
+catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
