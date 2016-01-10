1d0e05c70cd30256235992de7710bc02728c6c7d
Kiririn <ritsufag@gmail.com>
2016-01-10 14:45:12 +0300

    Fix silent args

--- pcsx2/tools/chocolateyinstall.ps1
+++ pcsx2/tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
   packageName   = 'pcsx2'
   fileType      = 'exe'
   url           = 'http://pcsx2.net/download/releases/windows/finish/6-windows/133-pcsx2-1-4-0-standalone-installer.html'
-  silentArgs    = '/verysilent'
+  silentArgs    = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
--- pcsx2/tools/chocolateyuninstall.ps1
+++ pcsx2/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName   = 'pcsx2'
 $fileType      = 'exe'
-$silentArgs    = '/verysilent'
-$file          = "$env:ProgramFiles(x86)\PCSX2 1.4.0\Uninst-pcsx2 1.4.0.exe"
+$silentArgs    = '/S'
+$file          = "$env:ProgramFiles (x86)\PCSX2 1.4.0\Uninst-pcsx2 1.4.0.exe"
 
 Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file
