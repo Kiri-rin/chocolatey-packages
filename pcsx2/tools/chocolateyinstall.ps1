6b7601543ac29867aaa0f093b818c4f4dc562492
Kiririn <ritsufag@gmail.com>
2016-01-10 13:36:21 +0300

    Added pcsx2 package

---
+++ pcsx2/tools/chocolateyinstall.ps1
@@ -0,0 +1,8 @@
+﻿$packageArgs = @{
+  packageName   = 'pcsx2'
+  fileType      = 'exe'
+  url           = 'http://pcsx2.net/download/releases/windows/finish/6-windows/133-pcsx2-1-4-0-standalone-installer.html'
+  silentArgs    = '/verysilent'
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ pcsx2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,6 @@
+﻿$packageName   = 'miranda-ng'
+$fileType      = 'exe'
+$silentArgs    = '/verysilent'
+$file          = "$env:ProgramFiles(x86)\PCSX2 1.4.0\Uninst-pcsx2 1.4.0.exe"
+
+Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file
