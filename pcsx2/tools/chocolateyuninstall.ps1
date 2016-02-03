2553b745cfe973b92ebccc9467b4984286f6b224
Kiririn <ritsufag@gmail.com>
2016-02-03 05:41:10 +0300

    Get install path from registry

--- pcsx2/tools/chocolateyuninstall.ps1
+++ pcsx2/tools/chocolateyuninstall.ps1
@@ -1,6 +1,17 @@
-﻿$packageName   = 'pcsx2'
+﻿function GetInstallDir {
+    $regPath = "HKCU:\SOFTWARE\PCSX2";
+    if (Test-Path $regPath) {
+        $regVal = Get-ItemProperty -Path $regPath -Name Install_Dir;
+        if (Test-Path $regVal.Install_Dir) {
+            return $regVal.Install_Dir;
+        }
+    }
+    return "$env:ProgramFiles (x86)\PCSX2 1.4.0";
+}
+
+$packageName   = 'pcsx2'
 $fileType      = 'exe'
 $silentArgs    = '/S'
-$file          = "$env:ProgramFiles (x86)\PCSX2 1.4.0\Uninst-pcsx2 1.4.0.exe"
+$file          = GetInstallDir + "\Uninst-pcsx2 1.4.0.exe"
 
 Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file
