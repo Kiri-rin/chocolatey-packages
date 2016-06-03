de33c5439c072147ecbb04994ebfd350ea58525c
Kiririn <ritsufag@gmail.com>
2016-06-03 23:39:56 +0300

    Update installer URI

--- pcsx2/tools/chocolateyinstall.ps1
+++ pcsx2/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageArgs = @{
   packageName   = 'pcsx2'
   fileType      = 'exe'
-  url           = 'http://pcsx2.net/download/releases/windows/finish/6-windows/133-pcsx2-1-4-0-standalone-installer.html'
+  url           = 'http://pcsx2.net/download/releases/windows/send/40-windows/118-pcsx2-1-4-0-setup.html'
   silentArgs    = '/S'
 }
 
