3a137dcff59f6f1a57625d6dd7a806174f4a6bee
Kiririn <ritsufag@gmail.com>
2016-06-05 00:44:51 +0300

    Rewrite uninstall script

--- pcsx2/tools/chocolateyuninstall.ps1
+++ pcsx2/tools/chocolateyuninstall.ps1
@@ -1,17 +1,14 @@
 ﻿function GetInstallDir {
     $regPath = "HKCU:\SOFTWARE\PCSX2";
+	$defPath = "$env:ProgramFiles (x86)\PCSX2 1.4.0";
     if (Test-Path $regPath) {
         $regVal = Get-ItemProperty -Path $regPath -Name Install_Dir;
         if (Test-Path $regVal.Install_Dir) {
             return $regVal.Install_Dir;
         }
     }
-    return "$env:ProgramFiles (x86)\PCSX2 1.4.0";
+    return $defPath;
 }
 
-$packageName   = 'pcsx2'
-$fileType      = 'exe'
-$silentArgs    = '/S'
-$file          = GetInstallDir + "\Uninst-pcsx2 1.4.0.exe"
-
-Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file
+$installDir = GetInstallDir
+Uninstall-ChocolateyPackage 'pcsx2' 'exe' '/S' "$(Join-Path $installDir 'Uninst-pcsx2 1.4.0.exe')"
