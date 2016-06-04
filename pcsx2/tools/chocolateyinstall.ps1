871b18374cd0374d142c18134da59365b9d3e11f
Kiririn <ritsufag@gmail.com>
2016-06-05 00:26:05 +0300

    Rewrite install script

--- pcsx2/tools/chocolateyinstall.ps1
+++ pcsx2/tools/chocolateyinstall.ps1
@@ -1,8 +1,1 @@
-﻿$packageArgs = @{
-  packageName   = 'pcsx2'
-  fileType      = 'exe'
-  url           = 'https://github.com/Kiri-rin/chocolatey-packages/raw/master/pcsx2/bin/pcsx2-1.4.0-setup.exe'
-  silentArgs    = '/S'
-}
-
-Install-ChocolateyPackage @packageArgs
+﻿Install-ChocolateyInstallPackage 'pcsx2' 'exe' '/S' "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)\pcsx2-1.4.0-setup.exe"
