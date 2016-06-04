fe5a50bfad9b6a940fa4ad48f74639f8224ffa1c
Kiririn <ritsufag@gmail.com>
2016-06-05 00:56:36 +0300

    Use Join-Path

--- pcsx2/tools/chocolateyuninstall.ps1
+++ pcsx2/tools/chocolateyuninstall.ps1
@@ -1,6 +1,6 @@
 ﻿function GetInstallDir {
-    $regPath = "HKCU:\SOFTWARE\PCSX2";
-	$defPath = "$env:ProgramFiles (x86)\PCSX2 1.4.0";
+	$defPath = Join-Path "$env:ProgramFiles (x86)" "PCSX2 1.4.0";
+	$regPath = "HKCU:\SOFTWARE\PCSX2";
     if (Test-Path $regPath) {
         $regVal = Get-ItemProperty -Path $regPath -Name Install_Dir;
         if (Test-Path $regVal.Install_Dir) {
