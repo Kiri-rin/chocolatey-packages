28634705fcfcfe873e6d8aae4ecc8550b06676be
Kiririn <ritsufag@gmail.com>
2016-06-20 00:28:32 +0300

    Update miranda-ng

--- miranda-ng/tools/chocolateyInstall.ps1
+++ miranda-ng/tools/chocolateyInstall.ps1
@@ -1,9 +1,14 @@
 ﻿$packageArgs = @{
   packageName   = 'miranda-ng'
   fileType      = 'exe'
-  url           = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4.exe'
-  url64bit      = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.4_x64.exe'
+  url           = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.5.exe'
+  url64bit      = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.5_x64.exe'
   silentArgs    = '/verysilent'
+}
+
+$installPath = "$env:ProgramFiles\Miranda NG"
+If (Test-Path "$installPath\*") {
+	Uninstall-ChocolateyPackage 'miranda-ng' 'exe' '/verysilent' "$(Join-Path $installPath 'Uninstall\unins000.exe')"
 }
 
 Install-ChocolateyPackage @packageArgs
--- miranda-ng/tools/chocolateyUninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿$packageName   = 'miranda-ng'
-$fileType      = 'exe'
-$silentArgs    = '/verysilent'
-$file          = "$env:ProgramFiles\Miranda NG\Uninstall\unins000.exe"
-
-Uninstall-ChocolateyPackage $packageName $fileType $silentArgs $file
