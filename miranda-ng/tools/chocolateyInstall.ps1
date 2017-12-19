989dc3e6cc01ef9c9ca1ab4ad56f5835a118e3d8
hey-red <hey.red@outlook.com>
2017-12-19 09:37:48 +0300

    Update miranda-ng package

--- miranda-ng/tools/chocolateyInstall.ps1
+++ miranda-ng/tools/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿$packageName = 'miranda-ng'
 $fileType    = 'exe'
-$url         = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.5.exe'
-$url64       = 'http://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.5_x64.exe'
+$url         = 'https://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.7.exe'
+$url64       = 'https://www.miranda-ng.org/distr/stable/miranda-ng-v0.95.7_x64.exe'
 $silentArgs  = '/verysilent'
  
 [array]$key = Get-UninstallRegistryKey -SoftwareName "Miranda NG"
